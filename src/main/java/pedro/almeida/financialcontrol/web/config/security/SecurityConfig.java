5cc075bb7e7de0d83214048df9eb95fa6bab471d
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-12-19 17:58:11 -0300

    feat: libera acesso a partir de qualquer URL para os métodos GET e POST

---
+++ src/main/java/pedro/almeida/financialcontrol/web/config/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package pedro.almeida.financialcontrol.web.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST").allowedHeaders("*").allowedOrigins("*");
+            }
+        };
+    }
+}
